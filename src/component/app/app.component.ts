05735bd13403ca301e6a02250d479a6a275a5459
KOKNE Kamal <KKE6@dsone.3ds.com>
2016-12-01 11:58:48 +0530

    Added time slots.

--- src/component/app/app.component.ts
+++ src/component/app/app.component.ts
@@ -7,10 +7,14 @@ import { TimeSlotComponent } from 'built/component/time-slot/time-slot.component
     directives: [TimeSlotComponent]
 })
 export class AppComponent {
-public Slots:Array = [
-        '9.00 - 10.00 AM',
-        '10.00 - 11.00 AM',
-        '11.00 - 12.00 AM',
-        '12.00 - 01.00 PM'
+    public Slots:Array = [
+        '9.00 AM - 10.00 AM',
+        '10.00 AM - 11.00 AM',
+        '11.00 AM - 12.00 PM',
+        '12.00 PM - 01.00 PM',
+        '01.00 PM - 02.00 PM',
+        '02.00 PM - 03.00 PM',
+        '03.00 PM - 04.00 PM',
+        '04.00 PM - 05.00 PM'
     ];
 }
